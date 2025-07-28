e016b3a7978e882ad55d1e8d72deb6b1e5161ea8
Kaur Kivilaan <kaurkivi@gmail.com>
2025-07-28 18:48:24 +0300

    Handle concurrent clients

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@
 
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
+use std::thread;
+use std::thread::JoinHandle;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -11,24 +13,37 @@ fn main() {
     
     let listener: TcpListener = TcpListener::bind("127.0.0.1:6379").unwrap();
     
-    for stream in listener.incoming() {
-        match stream {
+    let handles: Vec<JoinHandle<()>> = listener
+        .incoming()
+        .filter_map(|stream| match stream {
             Ok(stream) => {
                 println!("accepted new connection");
-                handle_client(stream);
+                Some(thread::spawn(move || {
+                    handle_client(stream);
+                }))
             }
             Err(e) => {
                 println!("error: {}", e);
+                None
             }
-        }
-    }
+        })
+        .collect();
+
+    handles
+        .into_iter()
+        .for_each(|handle: JoinHandle<()>| {
+            handle.join().unwrap()
+        });
 }
 
 fn handle_client(mut stream: TcpStream) {
-    loop {
-        let mut buffer = [0; 512];
-        stream.read(&mut buffer).unwrap();
-        
+    let mut buffer: [u8; 512] = [0; 512];
+
+    while let Ok(buffer_length) = stream.read(&mut buffer) {
+        if buffer_length == 0 {
+            break;
+        }
+
         stream.write_all(b"+PONG\r\n").unwrap();
     }
 }
