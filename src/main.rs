44c2fc8eb45dec546e9333894141f066529e8fab
Kaur Kivilaan <kaurkivi@gmail.com>
2025-07-28 19:06:57 +0300

    Handle concurrent clients with async

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,61 @@
 #![allow(unused_imports)]
 
-use std::io::{Read, Write};
-use std::net::{TcpListener, TcpStream};
-use std::thread;
-use std::thread::JoinHandle;
+// use std::io::{Read, Write};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+// use std::net::{TcpListener, TcpStream};
+use tokio::net::{TcpListener, TcpStream};
+use tokio::task::JoinHandle;
+// use std::thread;
+// use std::thread::JoinHandle;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
     // Uncomment this block to pass the first stage
     
-    let listener: TcpListener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    let listener: TcpListener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
     
-    let handles: Vec<JoinHandle<()>> = listener
-        .incoming()
-        .filter_map(|stream| match stream {
-            Ok(stream) => {
-                println!("accepted new connection");
-                Some(thread::spawn(move || {
-                    handle_client(stream);
-                }))
-            }
-            Err(e) => {
-                println!("error: {}", e);
-                None
-            }
-        })
-        .collect();
-
-    handles
-        .into_iter()
-        .for_each(|handle: JoinHandle<()>| {
-            handle.join().unwrap()
+    loop {
+        let (socket, _addr) = listener.accept().await.unwrap();
+        
+        tokio::spawn(async move {
+            handle_client(socket).await;
         });
+    }
+    
+//     let handles: Vec<JoinHandle<()>> = listener
+//         .incoming()
+//         .filter_map(|stream| match stream {
+//             Ok(stream) => {
+//                 println!("accepted new connection");
+//                 Some(thread::spawn(move || {
+//                     handle_client(stream);
+//                 }))
+//             }
+//             Err(e) => {
+//                 println!("error: {}", e);
+//                 None
+//             }
+//         })
+//         .collect();
+// 
+//     handles
+//         .into_iter()
+//         .for_each(|handle: JoinHandle<()>| {
+//             handle.join().unwrap()
+//         });
 }
 
-fn handle_client(mut stream: TcpStream) {
+async fn handle_client(mut stream: TcpStream) {
     let mut buffer: [u8; 512] = [0; 512];
 
-    while let Ok(buffer_length) = stream.read(&mut buffer) {
+    while let Ok(buffer_length) = stream.read(&mut buffer).await {
         if buffer_length == 0 {
             break;
         }
 
-        stream.write_all(b"+PONG\r\n").unwrap();
+        stream.write_all(b"+PONG\r\n").await.unwrap();
     }
 }
