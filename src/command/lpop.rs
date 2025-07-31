251cb1e7e979ef259abdbcf7ac55908f4a263b18
Kaur Kivilaan <kaurkivi@gmail.com>
2025-07-31 23:29:19 +0300

    Remove multiple elements

--- src/command/lpop.rs
+++ src/command/lpop.rs
@@ -4,16 +4,17 @@ use crate::key_value_store::KeyValueStore;
 
 pub struct LPopCommand {
     key: String,
+    amount: usize,
 }
 
 impl CommandRunner for LPopCommand {
     fn run(self: Box<Self>, store: &mut Box<dyn KeyValueStore>) -> Vec<u8> {
         store.get_mut(&self.key)
-            .and_then(|entity| { 
+            .and_then(|entity| {
                 entity.len().ok().filter(|&len| 0 < len)?;
-                entity.pop_front().ok() 
+                entity.pop_front(self.amount).ok()
             })
-            .map(|first_element| 
+            .map(|first_element|
                 format!("${}\r\n{}\r\n", first_element.len(), first_element)
                     .as_bytes()
                     .to_vec())
@@ -23,10 +24,21 @@ impl CommandRunner for LPopCommand {
 
 impl CommandRunnerFactory for LPopCommand {
     fn new(arguments: &[&str]) -> Result<Box<Self>, Error> {
-        if arguments.len() != 1 {
-            return Err(Error::new(ErrorKind::InvalidInput, "Expected a single argument"));
+        let argument_count = arguments.len();
+        if argument_count == 1 {
+            return Ok(Box::new(
+                LPopCommand { 
+                    key: String::from(arguments[0]), 
+                    amount: 1 }));
         }
-
-        Ok(Box::new(LPopCommand { key: String::from(arguments[0]) }))
+        if argument_count == 2 {
+            return Ok(Box::new(
+                LPopCommand {
+                    key: String::from(arguments[0]),
+                    amount: arguments[1].parse().unwrap()
+                }
+            ))
+        }
+        Err(Error::new(ErrorKind::InvalidInput, "Expected one or two arguments"))
     }
 }
